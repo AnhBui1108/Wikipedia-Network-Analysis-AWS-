aff661f53fcacd17fd7fe034ae6d4dd15f6b1ad9
AnhBui <anhbui@u.boisestate.edu>
2024-12-10 02:17:47 -0700

    correct test file, update synthetic dataset and refined main class

--- FinalTest.py
+++ FinalTest.py
@@ -45,7 +45,11 @@ def read_json(spark, path):
 exp_links= frozenset({
     frozenset([1,2]),
     frozenset([2, 8]),
-    frozenset([4,5]),
+    frozenset([8,5]),
+    frozenset([2,10]),
+    frozenset([2,7]),
+    frozenset([1,8]),
+    frozenset([11,12]),
     frozenset([4, 10])})
     
 
@@ -54,8 +58,8 @@ exp_links= frozenset({
 
 
 exp_components = frozenset({
-    frozenset([1, 2, 8]),
-    frozenset([4, 5, 10])
+    frozenset([1, 2, 4 ,5, 8, 7, 10]),
+    frozenset([11, 12])
 })
     
 
@@ -81,11 +85,14 @@ class TestMethods(unittest.TestCase):
             read_json(spark, 'redirect.jsonl')
         )
         dataIO.write(mutual_links, "mutual_links")
+        print("mutual link")
+        mutual_links.show()
         actual_link = frozenset(frozenset(r) for r in mutual_links.collect())
+        
         self.assertEqual(actual_link, exp_links)
 
         checkpoint_path = f"./test_output/"
-        cc = connected_components(spark, mutual_links, dataIO)
+        cc = connected_components(spark, mutual_links, dataIO, checkpoint_path)
         cc = cc.select(F.col("src").alias("vertex_ID"),
                F.col("updated_component_ID").alias("component_ID"))
         dataIO.write(cc, "test_components")
@@ -95,6 +102,7 @@ class TestMethods(unittest.TestCase):
                            .agg(F.expr("collect_list(vertex_ID) as members"))
                            .collect()
                           )
+        
         self.assertEqual(groups, exp_components)
         print("conected component stats")
         cc_stats = cc.groupBy("component_ID").agg(F.expr("count(*) as size"))
